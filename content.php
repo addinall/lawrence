0ec3ac6eee7f4ae15728af6b21dce540d986813e
addinall <addinall@addinall.net>
2014-08-20 19:00:34 +1000

    Change from HTML to PHP CMS (home grown) biggie...

---
+++ content.php
@@ -0,0 +1,106 @@
+
+<?php
+// vim: set expandtab tabstop=4 shiftwidth=4 autoindent smartindent:
+//---------------------------------------------------------
+// CAPTAIN  SLOG
+//---------------------------------------------------------
+//
+//	FILE:       content.php 
+//	SYSTEM:     Ridolfis' new system 
+//	AUTHOR:     Mark Addinall
+//	DATE:       20/08/2014
+//	SYNOPSIS:   Instead of burdening a simple little elegant system with a 
+//              BIG FAT UGLY waste of time like JOOMLA!, DJANGO or WordPress,
+//              we just stick in some content here that can be easily managed
+//              with a TEXT EDITOR.
+//
+//              **** DO NOT USE WORD!!! ******
+//
+//------------+-------------------------------+------------
+// DATE       |    CHANGE                     |    WHO
+//------------+-------------------------------+------------
+//
+//
+
+
+
+// First the header conten
+
+$logo = "Lawrence Ridolfi";
+
+$header_message = <<<END
+My new web page.  My on-call free Web designer does not know what it is for yet. I will tell him later when delivering drinks. He now be choosing sick colour schemes.
+END;
+
+// now before anyone starts screaming "USE AN ARRAY, OR AN OBJECT CONTAINING AN ARRAY, OR AN
+// ARRAY OF OBJECTS",  I am not giving mu customer lessons in CompSci.  I want him, her or
+// to easily understand that if you changed the text in paragraph one, it will reflect
+// on the screen in paragraph one.  That is ALL I want this file to do.  So fuck off, weenies.
+
+$header_paragraph = <<<END
+Dunkle priming; crystal malt, " degrees plato top-fermenting yeast bacterial length mouthfeel." cask conditioning seidel heat exchanger sour/acidic mouthfeel lager hops. krausen berliner weisse dry stout yeast copper. trappist dry stout pilsner brew dry stout. aerobic fermentation bottom fermenting yeast aroma hops. malt cask conditioning.
+END;
+
+// This is the END of the HEADER CONTENT
+
+// These are the various HEADINGS in the system
+// If you want to INCREASE the number of paragraphs, you need to understand index.php
+
+$sidebar_header = "Play with me!";
+
+$heading_one    = "I'm serious as a heart attack";
+$heading_two    = "Your bones don't break, mine do";
+$heading_three  = "The trouble with Tribbles is";
+$heading_four   = "And back in Parramatta";
+
+$footer_heading = "Shoot that bitch in the kneecaps";
+
+// This is the END of the HEADINGS
+// If you want to get rid of one or more, make them an empty string = "";
+// If you want to add more paragraphs, that is more complex.  Ask first!  ;-)
+
+
+// This is the START of the SIDEBAR and MAIN CONTENT
+
+
+$sidebar_paragraph = <<<END
+Now that there is the Tec-9, a crappy spray gun from South Miami. This gun is advertised as the most popular gun in American crime. Do you believe that shit? It actually says that in the little book that comes with it: the most popular gun in American crime. Like they're actually proud of that shit.
+END;
+
+$paragraph_one = <<<END
+Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it? Do you see a little Asian child with a blank expression on his face sitting outside on a mechanical helicopter that shakes when you put quarters in it? That's what you see at a toy store.
+END;
+
+$paragraph_two = <<<END
+Face like a slapped arse. Bloomin' 'eck dahn t'coil oil soft southern pansy gi' o'er. Ee by gum what's that when it's at ooam appens as maybe michael palin mardy bum. Bloomin' 'eck ee by gum wacken thi sen up where's tha bin. Th'art nesh thee where there's muck there's brass that's champion soft southern pansy eeh chuffin' nora. Aye t'foot o' our stairs. Be reet ah'll learn thi ey up tha what where there's muck there's brass. Is that thine. Tha daft apeth ah'll gi' thi summat to rooer abaht wacken thi sen up god's own county michael palin tha what.
+END;
+
+$paragraph_three = <<<END
+The game's not big enough unless it scares you a little. I think you've let your personal feelings cloud your judgement. We know you're dealing in stolen ore. But I wanna talk about the assassination attempt on Lieutenant Worf. The look in your eyes, I recognize it. You used to have it for me. I am your worst nightmare! Talk about going nowhere fast. Mr. Worf, you do remember how to fire phasers? Wait a minute - you've been declared dead. You can't give orders around here. 
+END;
+
+$paragraph_four = <<<END
+We're going strewth when lets throw a truckie. He hasn't got a moolah flamin lets get some postie. Lets get some dropkick no worries as stands out like freckle. It'll be chuck a yewy flamin lets get some yabber. As busy as a turps as busy as a lippy.
+END;
+
+$footer_paragraph = <<<END
+Lorizzle ipsum dolor check it out amizzle, go to hizzle adipiscing elit. Nullizzle bow wow wow phat, my shizz volutpat, get down get down quis, dope vel, own yo'. Pellentesque phat tortor. Sed funky fresh. Fusce izzle dolor dapibus pot tempizzle bow wow wow. Dope pellentesque nibh et shut the shizzle up. Break it down izzle dang. Pellentesque eleifend i saw beyonces tizzles and my pizzle went crizzle bling bling. In hac sizzle platea dope. Bow wow wow dapibizzle. Curabitur cool ass, pretium eu, mattizzle izzle, get down get down daahng dawg, nunc. Phat suscipizzle. Integer semper we gonna chung sizzle purus.
+END;
+
+$footer_legals = <<<END
+<small>2014 <span class="license">Created by <a href="http://www.addinall.net" target="_blank">Mark Addinall</a> <span class="amp">&amp;</span> <a href="http://www.crosscitydjs.com.au" target="_blank">Lawrence Ridolfi</a> Well, mostly Mark Addinall.  Nearly all of it in fact.  Using Marks NEW SOOPER DOOPER RESPONSIVE boilerplate for FondleSlabs.  Neat</span>. </small>
+END;
+
+
+$footer_javascript = <<<END
+    <!-- Minimized jQuery from Google CDN -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+    <!-- HTML5 IE Enabling Script -->
+    <!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+    <!-- CSS3 Media Queries -->
+    <script src="js/respond.min.js"></script>
+    <!-- Optimized Google Analytics. Change UA-XXXXX-X to your site ID -->
+    <script>var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.src='//www.google-analytics.com/ga.js';s.parentNode.insertBefore(g,s)}(document,'script'))</script>
+END;
+
+?>
---
+++ index.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="en">
+<!--
+ vim: set expandtab tabstop=4 shiftwidth=4 autoindent smartindent:
+// CAPTAIN  SLOG
+//
+//  FILE:       index.php 
+//  SYSTEM:     New page for Ridolfi 
+//  AUTHOR:     Mark Addinall
+//  DATE:       19/08/2014
+//  SYNOPSIS:   In the words of Ray Charles, "an I guess I'll take
+                another IOU...."
+
+    This page in all of it's manifistations is RESPONSIVE and FLUID.
+    It will work on a PC, Fondleslab or telephone.  Perhaps not a
+    Rotary Telephone.
+
+    Everything in this site is built responsive from the ground up,
+    the slider, the forms, image containers, movie containers.
+
+    20/08/2014 -    Changed the file from .html to .php to indicate that we will
+                    now be managing the content through some VERY simplistic
+                    PHP inline code.  This gives Apache for warning to load the
+                    PHP modules, speeding processing.
+-->
+
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# website: http://ogp.me/ns/website#">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+<meta name="description" content="A web site created for me.">
+<meta name="keywords"  content="HTML5, CSS3, PHP, Responsive, Fluid, Web, Design, Modern">
+<meta name="author" content="Mark Addinall.  Brisbane. Australia">
+<meta name="robots" content="all">
+
+<title>Lawrence Ridolfi</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+
+
+    
+<!-- Place favicon.ico and apple-touch-icon.png in root directory -->
+        
+<link href="css/style.css" rel="stylesheet" />
+
+<link rel="stylesheet" href="AudioPlayer/css/audioplayer.css" />
+
+<script src="AudioPlayer/js/jquery.js"></script>
+<script src="AudioPlayer/js/audioplayer.js"></script>
+
+<script>$( function() { $( 'audio' ).audioPlayer(); } );</script>
+
+<?php require("content.php");   ?>
+
+
+
+</head>
+
+<body>
+    <header>
+        <div class="g1">
+            <h1><?php echo $logo;  ?></h1>
+        </div>
+        <div class="g2">
+            <p></p>
+            <h5>
+            <?php echo $header_message; ?>
+            </h5>
+        </div>
+        <div class="g2">
+            <p>
+            <?php echo $header_paragraph; ?>
+            </p>
+        </div>
+    </header>
+    <div class="cf"></div>
+    <div id="content">
+        <div class="g1">
+            <h3><?php echo $sidebar_header; ?></h3>
+            <p>
+                <?php echo $sidebar_paragraph; ?>
+            </p>
+
+    <!-- That was the end of the actual HUMAN editable content for the sidebar.
+        In this installation the rest of this sidebar is full of AUDIO machines.
+        It might not be a great idea to edit them unless you are pretty sure
+        of what is going on!  -->
+
+            <h5>Juicy</h5>
+            <p>
+                <audio src="AudioPlayer/audio/juicy.mp3" preload="none" controls></audio>
+            </p>
+            <h5>Boss Nigger</h5>
+            <p>
+                <audio src="AudioPlayer/audio/bn.mp3" preload="none" controls></audio>
+            </p>
+            <h5>Hunger Games</h5>
+            <p>
+                <audio src="AudioPlayer/audio/dghg.mp3" preload="none" controls></audio>
+            </p>
+            <h5>Circles</h5>
+            <p>
+                <audio src="AudioPlayer/audio/KDrew_Circles.mp3" preload="none" controls></audio>
+            </p>
+            <h5>(FourFlossFive)Six</h5>
+            <p>
+	            <audio preload="none" controls>
+		            <source src="AudioPlayer/audio/BlueDucks_FourFlossFiveSix.mp3">
+		            <source src="AudioPlayer/audio/BlueDucks_FourFlossFiveSix.ogg">
+		            <source src="AudioPlayer/audio/BlueDucks_FourFlossFiveSix.wav">
+	            </audio>
+            </p>
+        </div>
+
+        <!-- That is the end of the side bar.  REMEMBER, this is a FLUID and
+             RESPONSIVE GRID system that will allocate space as it sees available.
+             The bit we just finished behaves as a LEFT SIDE BAR on full size
+             system!!!   -->
+
+
+        <div class="g2">
+            <h3><?php echo $heading_one; ?></h3>
+            <p>
+                <?php echo $paragraph_one; ?>
+            </p>
+            <h3><?php echo $heading_two; ?></h3>
+            <p>
+                <?php echo $paragraph_two; ?>
+            </p>
+            <h3><?php echo $heading_three; ?></h3>
+            <p>
+                <?php echo $paragraph_three; ?>
+            </p>
+            <h3><?php echo $heading_four; ?></h3>
+            <p>
+                <?php echo $paragraph_four; ?>
+            </p>
+        </div>
+        <div class="g3">
+            <h3><?php echo $footer_heading; ?></h3>
+            <p>
+                <?php echo $footer_paragraph; ?>
+            </p>
+        </div>
+    </div>
+    <footer class="g3 cf">
+        <hr>
+            <?php echo $footer_legals; ?>
+        <hr>
+    </footer>
+
+    <!-- JavaScript at the bottom for fast page loading -->
+
+    <?php echo $footer_javascript; ?>
+
+</body>
+</html>
